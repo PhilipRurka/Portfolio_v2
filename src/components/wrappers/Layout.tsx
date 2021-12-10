bdb3f50d6bf3f42c042709ff5329b2bf3852baad
Philip Rurka <hey@philiprurka.com>
2021-12-10 13:45:13 -0400

    Setup - Re structuring some folders a bit

--- src/components/wrappers/Layout.tsx
+++ src/components/wrappers/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Reset } from "../../styled/base";
-import { Header } from "../Header";
+import { Header } from "../header/Header.container";
 
 const Layout: FC<React.ReactNode> = (props) => {
   const { children } = props
